4a21152e54c5ac785121a30a188dd088a3ebe108
YuriyNik156 <yuriy.nik156@gmail.com>
2025-05-16 15:38:05 +0300

    add line_graph.py, histogram.py and scatter_diagram.py

---
+++ scatter_diagram.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+x = [1, 4, 6, 7]
+y = [3, 5, 8, 10]
+
+plt.scatter(x, y)
+
+plt.xlabel("ось X")
+plt.ylabel("ось Y")
+plt.title("тестовая диаграмма рассеяния")
+
+plt.show()
